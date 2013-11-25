03e50416a114e233b2a1e57d792504cf501b2877
Nelian Colon <nelian.colon@upr.edu>
2013-11-24 22:01:42 -0400

    sorting mejorado

--- HH3/sorting/Sorting.java
+++ HH3/sorting/Sorting.java
@@ -1,9 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Sorting {
-	public static void main(String[] args){
+	public static void main(String[] args) throws NumberFormatException, IOException{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		do{
-			list.add(Integer.nextInt());
+			list.add(Integer.parseInt(reader.readLine()));
 		} while(reader.ready());
 
 		Collections.sort(list);
